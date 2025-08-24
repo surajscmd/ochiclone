503b9ff828bedf194699983fb3e27d0bcafb2a95
suraj <surajraj.shku1999@gmail.com>
2025-08-24 14:26:09 +0530

    scroll-import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Fullpage from './elements/Fullpage'
 import LocomotiveScroll from 'locomotive-scroll';
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
 function App() {
 
